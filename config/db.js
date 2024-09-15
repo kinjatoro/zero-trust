622c20be43169aee2d7b6c737ab42dd58fd912e4
yaguecarp <nyaguenasif@gmail.com>
2024-09-15 18:43:39 -0300

    new db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 import mysql from 'mysql2'
 
 export const connection = mysql.createConnection({
-    host: 'database-1.cf0ei60qeumd.us-east-2.rds.amazonaws.com',
-    user: 'admin',
-    password: 'password',
-    database: 'mydb',
-    port: 3306 // El puerto por defecto para MySQL
+    // host: 'database-1.cf0ei60qeumd.us-east-2.rds.amazonaws.com',
+    // user: 'admin',
+    // password: 'password',
+    // database: 'mydb',
+    // port: 3306 // El puerto por defecto para MySQL
+    host: "galicia-rds.czjxktqmtnno.us-east-1.rds.amazonaws.com",
+    port: "3306",
+    user: "admin",
+    password: "password",
+    database: "galicia",
 })
 
 connection.connect(err => {
